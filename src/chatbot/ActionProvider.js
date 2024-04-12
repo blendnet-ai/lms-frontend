979d43475ea2af57cde2021808bf58effa5a9992
Vitika Soni <vitikasoni@Vitikas-MacBook-Pro.local>
2024-04-12 13:58:44 +0530

    Implement bot response wait msg (...)

--- src/chatbot/ActionProvider.js
+++ src/chatbot/ActionProvider.js
@@ -1,6 +1,9 @@
 import React, { useEffect, useContext } from "react";
 import { BotContext } from "../pages/Learning";
 import { auth } from "../configs/firebase";
+
+const RESPONSE_WAIT_MSG = "...";
+
 const ActionProvider = ({ createChatBotMessage, setState, children }) => {
   const { ws, videoId } = useContext(BotContext);
 
@@ -16,10 +19,29 @@ const ActionProvider = ({ createChatBotMessage, setState, children }) => {
   ws.onmessage = (event) => {
     const botMessage = createChatBotMessage(JSON.parse(event.data).message);
 
-    setState((prev) => ({
-      ...prev,
-      messages: [...prev.messages, botMessage],
-    }));
+    setState((prev) => {
+      const lastMessageIndex = prev.messages.length - 1;
+      const lastMessage = prev.messages[lastMessageIndex];
+
+      console.log(lastMessage);
+
+      if (
+        lastMessage.message === RESPONSE_WAIT_MSG &&
+        lastMessage.type === "bot"
+      ) {
+        // If the last message is RESPONSE_WAIT_MSG and is a bot message then remove it and add the received botMessage
+        return {
+          ...prev,
+          messages: [...prev.messages.slice(0, -1), botMessage],
+        };
+      } else {
+        // If the last message is not "..." then just add the received botMessage
+        return {
+          ...prev,
+          messages: [...prev.messages, botMessage],
+        };
+      }
+    });
   };
 
   const handleSendWsMsg = async (message) => {
@@ -30,6 +52,15 @@ const ActionProvider = ({ createChatBotMessage, setState, children }) => {
         token: await auth.currentUser.getIdToken(),
       })
     );
+
+    const botDotMsg = createChatBotMessage(RESPONSE_WAIT_MSG, {
+      loading: true,
+    });
+
+    setState((prev) => ({
+      ...prev,
+      messages: [...prev.messages, botDotMsg],
+    }));
   };
 
   // Put the handleHello function in the actions object to pass to the MessageParser
