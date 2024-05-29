6f0402cc61ce6383a4a08d397bc8236cdd4ec876
Vitika Soni <vitikasoni@Vitikas-MacBook-Pro.local>
2024-05-29 13:24:01 +0530

    TestWelcome: Implement buffer layout

--- src/pages/TestWelcome.tsx
+++ src/pages/TestWelcome.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import Header from "../components/Header";
 import "./../styles/TestWelcome.css";
 import { HomeHeaderContent } from "./Home";
@@ -15,8 +15,21 @@ type TestWelcomeProps = {
   assessment_generation_id: number;
 };
 
+enum Layouts {
+  WELCOME = 0,
+  BUFFER = 1,
+}
+
 export default function TestWelcome(props: TestWelcomeProps) {
   const [name, setName] = useState("");
+
+  const [currentLayout, setCurrentLayout] = useState(Layouts.WELCOME);
+
+  const [timeLeft, setTimeLeft] = useState(10);
+
+  const timerRef = useRef<NodeJS.Timeout | null>(null);
+
+  const [assessmentId, setAssessmentId] = useState<number | null>(null);
 
   useEffect(() => {
     auth.onAuthStateChanged((user) => {
@@ -24,13 +37,47 @@ export default function TestWelcome(props: TestWelcomeProps) {
     });
   }, []);
 
+  useEffect(() => {
+    return () => {
+      if (timerRef.current) {
+        clearInterval(timerRef.current);
+      }
+    };
+  }, []);
+
+  useEffect(() => {
+    if (timeLeft <= 0) handleStartTestClick();
+  }, [timeLeft]);
+
   const navigate = useNavigate();
+
+  const handleTakeATestClick = async () => {
+    setCurrentLayout(Layouts.BUFFER);
+
+    await startAttempt();
+
+    timerRef.current = setInterval(() => {
+      setTimeLeft((prevTimeLeft) => {
+        if (prevTimeLeft <= 1) {
+          clearInterval(timerRef.current!);
+          return 0;
+        }
+        return prevTimeLeft - 1;
+      });
+    }, 1000);
+  };
 
   const startAttempt = async () => {
     const response = await EvalAPI.startAssessment(
       props.assessment_generation_id
     );
-    navigate(`/${props.testRoutePath}?assessment_id=${response.assessment_id}`);
+
+    setAssessmentId(response.assessment_id);
+  };
+
+  const handleStartTestClick = () => {
+    if (!assessmentId) return;
+    navigate(`/${props.testRoutePath}?assessment_id=${assessmentId}`);
   };
 
   return (
@@ -45,18 +92,41 @@ export default function TestWelcome(props: TestWelcomeProps) {
         }
       />
       <div className="comm-test-wel-content">
-        <img
-          className="comm-test-wel-illustration"
-          src={`/illustrations/${props.illustration}`}
-          alt=""
-        />
-        <div className="comm-test-wel-content-heading">
-          {props.headingInner}
-        </div>
-        <div className="comm-test-wel-content-des">
-          <div className="comm-test-wel-content-des-inner">{props.content}</div>
-        </div>
-        <button onClick={startAttempt}>Take a test</button>
+        {currentLayout == Layouts.WELCOME && (
+          <>
+            <img
+              className="comm-test-wel-illustration"
+              src={`/illustrations/${props.illustration}`}
+              alt=""
+            />
+            <div className="comm-test-wel-content-heading">
+              {props.headingInner}
+            </div>
+            <div className="comm-test-wel-content-des">
+              <div className="comm-test-wel-content-des-inner">
+                {props.content}
+              </div>
+            </div>
+            <button onClick={handleTakeATestClick}>Take a test</button>
+          </>
+        )}
+        {currentLayout == Layouts.BUFFER && (
+          <>
+            <div className="CommunicationTestWelcome-timer-container">
+              <img
+                className="CommunicationTestWelcome-hourglass"
+                src="/illustrations/hourglass.svg"
+                alt=""
+              />
+              <div className="CommunicationTestWelcome-time">
+                {timeLeft} sec
+              </div>
+            </div>
+            <div>Your test will start soon</div>
+            <div className="CommunicationTestWelcome-atb">All the best</div>
+            <button onClick={handleStartTestClick}>Start Test</button>
+          </>
+        )}
       </div>
     </div>
   );
