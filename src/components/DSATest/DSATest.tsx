37d7bf25174e62527cf3d8bb33f24de03aadb646
Vitika Soni <vitikasoni@Vitikas-MacBook-Pro.local>
2024-07-01 14:12:56 +0530

    DSATest: Implement displaying question in HTML

--- src/components/DSATest/DSATest.tsx
+++ src/components/DSATest/DSATest.tsx
@@ -1,10 +1,25 @@
 import { useEffect, useRef, useState } from "react";
 import Editor, { OnMount } from "@monaco-editor/react";
 import * as monaco from "monaco-editor";
-import { Drawer } from "@mui/material";
+import { Box, Drawer } from "@mui/material";
 import useResize from "../../hooks/useResize";
+import "./DSATest.css";
 
-export default function DSATest() {
+type DSATestData = {
+  question: string;
+  title: string;
+};
+
+export default function DSATestWrapper() {
+  const data = {
+    title: "Two Sum",
+    question:
+      '<p>Given an array of integers <code>nums</code>&nbsp;and an integer <code>target</code>, return <em>indices of the two numbers such that they add up to <code>target</code></em>.</p>\n\n<p>You may assume that each input would have <strong><em>exactly</em> one solution</strong>, and you may not use the <em>same</em> element twice.</p>\n\n<p>You can return the answer in any order.</p>\n\n<p>&nbsp;</p>\n<p><strong class="example">Example 1:</strong></p>\n\n<pre>\n<strong>Input:</strong> nums = [2,7,11,15], target = 9\n<strong>Output:</strong> [0,1]\n<strong>Explanation:</strong> Because nums[0] + nums[1] == 9, we return [0, 1].\n</pre>\n\n<p><strong class="example">Example 2:</strong></p>\n\n<pre>\n<strong>Input:</strong> nums = [3,2,4], target = 6\n<strong>Output:</strong> [1,2]\n</pre>\n\n<p><strong class="example">Example 3:</strong></p>\n\n<pre>\n<strong>Input:</strong> nums = [3,3], target = 6\n<strong>Output:</strong> [0,1]\n</pre>\n\n<p>&nbsp;</p>\n<p><strong>Constraints:</strong></p>\n\n<ul>\n\t<li><code>2 &lt;= nums.length &lt;= 10<sup>4</sup></code></li>\n\t<li><code>-10<sup>9</sup> &lt;= nums[i] &lt;= 10<sup>9</sup></code></li>\n\t<li><code>-10<sup>9</sup> &lt;= target &lt;= 10<sup>9</sup></code></li>\n\t<li><strong>Only one valid answer exists.</strong></li>\n</ul>\n\n<p>&nbsp;</p>\n<strong>Follow-up:&nbsp;</strong>Can you come up with an algorithm that is less than <code>O(n<sup>2</sup>)</code><font face="monospace">&nbsp;</font>time complexity?',
+  };
+  return <DSATest {...data} />;
+}
+
+function DSATest(props: DSATestData) {
   const editorRef = useRef<monaco.editor.IStandaloneCodeEditor | null>(null);
   const { width: questionDrawerWidth, enableResize } = useResize({
     startWidth: window.innerWidth * 0.5,
@@ -37,6 +52,14 @@ export default function DSATest() {
         variant="permanent"
         anchor="left"
       >
+        <Box sx={{ padding: "10px" }}>
+          <h2>{props.title}</h2>
+          <div
+            dangerouslySetInnerHTML={{
+              __html: props.question,
+            }}
+          />
+        </Box>
         <div
           style={{
             position: "absolute",
@@ -63,13 +86,15 @@ export default function DSATest() {
         variant="permanent"
         anchor="right"
       >
-        <Editor
-          width={editorDrawerWidth}
-          height="90vh"
-          defaultLanguage="java"
-          defaultValue="// some comment"
-          onMount={handleEditorDidMount}
-        />
+        <Box sx={{ padding: "10px" }}>
+          <Editor
+            width={editorDrawerWidth}
+            height="90vh"
+            defaultLanguage="java"
+            defaultValue="// some comment"
+            onMount={handleEditorDidMount}
+          />
+        </Box>
       </Drawer>
     </>
   );
