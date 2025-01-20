abee4fcffd5a36d5e4839362da1292ac583b1dae
Abhip32 <abhishek.a.patil112@gmail.com>
2025-01-20 16:25:49 +0530

    form for no role users added on no role screen

--- src/pages/NoRole.tsx
+++ src/pages/NoRole.tsx
@@ -37,9 +37,14 @@ function NoRole({ userData }: NoRoleProps) {
           borderLeft: "5px solid #f44336",
         }}
       >
-        Sorry, <strong>{userData}</strong> is not registered with us. Please
+        {/* Sorry, <strong>{userData}</strong> is not registered with us. Please
         contact your institute to get your email registered, or try signing in
-        with a different email.
+        with a different email. */}
+        {/* In case of no role ask user to fill a google from and wait for response */}
+        Sorry, your email ID <strong>{userData}</strong> is not registered with us. 
+        Kindly fill out this <a href="https://docs.google.com/forms/d/1xP7BOFES7ZAO8gRhJaDCDFZnj09xe0x03uls3ZTnujg/edit">form</a> to provide additional details and wait for a response from our team.
+        <br/>
+        <a href="https://docs.google.com/forms/d/1xP7BOFES7ZAO8gRhJaDCDFZnj09xe0x03uls3ZTnujg/edit">Google Form Link</a>
       </Typography>
 
       {/* logout button  */}
