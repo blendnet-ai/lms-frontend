4d8e58ba90cb5e34225e06d48dcc61310e25aae5
Yasir Mansoori <yasir.mansoori000@gmail.com>
2025-01-27 17:40:06 +0530

    Refactor CreateNotificationModal title; update Homepage to manage loading state and error handling for live class details
    Bug : Have to fix the edit modal max stack reached error

--- src/modals/EditLiveClassModal.tsx
+++ src/modals/EditLiveClassModal.tsx
@@ -1,77 +1,83 @@
-import {
-  Backdrop,
-  Box,
-  Button,
-  Fade,
-  FormControl,
-  InputLabel,
-  MenuItem,
-  Modal,
-  Select,
-  TextField,
-  Typography,
-} from "@mui/material";
-import { useState } from "react";
+import { Fragment, useState, useEffect } from "react";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import LiveClassAPI from "../apis/LiveClassAPI";
+import formatDate from "@/utils/formatDate";
+import formatTimeUsingDate from "@/utils/formatTimeUsingDate";
 import {
-  DatePicker,
-  LocalizationProvider,
-  TimePicker,
-} from "@mui/x-date-pickers";
-import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
-import dayjs, { Dayjs } from "dayjs";
-
-type EditLiveClassModalProps = {
-  open: boolean;
-  close: () => void;
-  submit: () => void;
-  data: any;
-  meetingId: string;
-  isLiveClassUpdated: (value: boolean) => void;
-};
-
-interface ErrorField {
-  startDate: string | null;
-  startTime: string | null;
-  duration: string | null;
-}
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+import { CalendarIcon } from "lucide-react";
+import { format } from "date-fns";
+import { Calendar } from "@/components/ui/calendar";
+import { TimePickerAMPM } from "@/components/Custom/TimePickerAMPM";
+import { TimePicker } from "@/components/Custom/TimePicker";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+import {
+  ApiResponse,
+  EditLiveClassModalProps,
+  ErrorFieldEditModal,
+} from "./types";
 
 const EditLiveClassModal = (props: EditLiveClassModalProps) => {
-  // const [startDate, setStartDate] = useState<Dayjs | null>(null);
-  // const [startTime, setStartTime] = useState<Dayjs | null>(null);
-  // const [duration, setDuration] = useState<Dayjs | null>(null);
+  const [startDate, setStartDate] = useState<Date | undefined>(
+    props.data?.data?.start_date
+      ? new Date(props.data.data.start_date)
+      : undefined
+  );
+  const [startTime, setStartTime] = useState<Date | undefined>(
+    props.data?.data?.start_time
+      ? new Date(`1970-01-01T${props.data.data.start_time}`)
+      : undefined
+  );
+  const [duration, setDuration] = useState<Date | undefined>(
+    props.data?.data?.duration
+      ? new Date(`1970-01-01T${props.data.data.duration}`)
+      : undefined
+  );
 
-  const [dateTimeData, setDateTimeData] = useState<{
-    startDate: Dayjs | null;
-    startTime: Dayjs | null;
-    duration: Dayjs | null;
-  }>({
+  const [errorField, setErrorField] = useState<ErrorFieldEditModal>({
     startDate: null,
     startTime: null,
     duration: null,
   });
 
-  const [errorField, setErrorField] = useState<ErrorField>({
-    startDate: null,
-    startTime: null,
-    duration: null,
-  });
-
-  const formatDate = (date: Date): string => {
-    const year = date.getFullYear();
-    const month = String(date.getMonth() + 1).padStart(2, "0");
-    const day = String(date.getDate()).padStart(2, "0");
-
-    return `${year}-${month}-${day}`;
-  };
+  useEffect(() => {
+    if (props.data?.data) {
+      setStartDate(
+        props.data.data.start_date
+          ? new Date(props.data.data.start_date)
+          : undefined
+      );
+      setStartTime(
+        props.data.data.start_time
+          ? new Date(`1970-01-01T${props.data.data.start_time}`)
+          : undefined
+      );
+      setDuration(
+        props.data.data.duration
+          ? new Date(`1970-01-01T${props.data.data.duration}`)
+          : undefined
+      );
+    }
+  }, [props.data]);
 
   const validateFields = () => {
-    const errors: ErrorField = {
-      startDate: !dateTimeData.startDate ? "Start date is required" : null,
-      startTime: !dateTimeData.startTime ? "Start time is required" : null,
-      duration: !dateTimeData.duration ? "Duration is required" : null,
+    const errors: ErrorFieldEditModal = {
+      startDate: startDate ? null : "Start date is required",
+      startTime: startTime ? null : "Start time is required",
+      duration: duration ? null : "Duration is required",
     };
 
     setErrorField(errors);
@@ -82,31 +88,25 @@ const EditLiveClassModal = (props: EditLiveClassModalProps) => {
     e.preventDefault();
 
     if (!validateFields()) {
-      return
+      return;
     }
 
     const refactoredFormData = {
-      start_date: formatDate(dateTimeData.startDate!.toDate()),
-      start_time: dateTimeData.startTime!.format("HH:mm:ss"),
-      duration: dateTimeData.duration!.format("HH:mm:ss"),
+      start_date: formatDate(startDate as Date),
+      start_time: formatTimeUsingDate(startTime as Date),
+      duration: formatTimeUsingDate(duration as Date),
     };
 
     try {
-      const response = await LiveClassAPI.updateLiveClass(
+      const response: ApiResponse = await LiveClassAPI.updateLiveClass(
         refactoredFormData,
         Number(props.meetingId)
       );
+
       if (response.message) {
         props.close();
-        setDateTimeData({
-          startDate: null,
-          startTime: null,
-          duration: null,
-        });
-        setTimeout(() => {
-          toast.success("Live class updated successfully", { theme: "dark" });
-        }, 500);
         props.isLiveClassUpdated(true);
+        toast.success("Live class updated successfully", { theme: "dark" });
       } else {
         throw new Error("Error updating live class");
       }
@@ -116,208 +116,116 @@ const EditLiveClassModal = (props: EditLiveClassModalProps) => {
   };
 
   return (
-    <>
-      <Modal
-        aria-labelledby="create-live-class-modal-title"
-        aria-describedby="create-live-class-modal-description"
-        open={props.open}
-        onClose={props.close}
-        closeAfterTransition
-        slots={{ backdrop: Backdrop }}
-        slotProps={{
-          backdrop: {
-            timeout: 500,
-          },
-        }}
+    <Fragment>
+      <Dialog
+        open={props.open !== undefined ? props.open : true}
+        onOpenChange={() => props.close()}
       >
-        <Fade in={props.open}>
-          <Box
-            sx={{
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "center",
-              position: "absolute",
-              top: "50%",
-              left: "50%",
-              transform: "translate(-50%, -50%)",
-              width: 900,
-              height: 700,
-              overflowY: "auto",
-              backgroundColor: "#FFFFFF",
-              boxShadow: "0px 4px 4px 0px #205EFF26",
-              p: 4,
-              borderRadius: 2,
-            }}
-          >
-            <Box
-              sx={{
-                display: "flex",
-                flexDirection: "column",
-                gap: "20px",
-                width: "100%",
-                // padding: "20px",
-              }}
-            >
-              <Box
-                sx={{
-                  display: "flex",
-                  flexDirection: "row",
-                  justifyContent: "space-between",
-                  alignItems: "center",
-                  gap: "20px",
-                  width: "100%",
-                }}
-              >
-                <Typography
-                  sx={{
-                    fontSize: "24px",
-                    color: "#2059EE",
-                    fontWeight: "bold",
-                    marginBottom: "20px",
-                  }}
-                >
-                  Edit Live Class
-                </Typography>
-              </Box>
-              <Box
-                component="form"
-                onSubmit={handleSubmitData}
-                sx={{
-                  display: "flex",
-                  flexDirection: "column",
-                  gap: "1rem",
-                  width: "100%",
-                }}
-              >
-                {/* Title  */}
-                <TextField
-                  label="Title"
-                  size="small"
-                  placeholder="SQL Class"
-                  type="text"
-                  variant="outlined"
-                  fullWidth
-                  required
-                  value={props.data.data?.title}
-                  disabled
-                />
-                {/* Start Date */}
-                <LocalizationProvider dateAdapter={AdapterDayjs}>
-                  <DatePicker
-                    label="Start Date*"
-                    value={dateTimeData.startDate}
-                    disablePast
-                    onChange={(date) => {
-                      setDateTimeData({ ...dateTimeData, startDate: date });
-                      setErrorField({ ...errorField, startDate: null });
-                    }}
-                  />
-                  {errorField.startDate && (
-                    <Typography
-                      sx={{
-                        fontSize: "12px",
-                        fontWeight: "bold",
-                        color: "#FF0000",
-                      }}
-                    >
-                      {errorField.startDate}
-                    </Typography>
-                  )}
-                </LocalizationProvider>
-                {/* End Date */}
-                <LocalizationProvider dateAdapter={AdapterDayjs}>
-                  <DatePicker
-                    value={dayjs(props.data.data?.end_date)}
-                    label="End Date"
-                    disabled
-                  />
-                </LocalizationProvider>
-                {/* start Time  */}
-                <LocalizationProvider dateAdapter={AdapterDayjs}>
-                  <TimePicker
-                    label="Start Time*"
-                    value={dateTimeData.startTime}
-                    views={["hours", "minutes", "seconds"]}
-                    ampm={false}
-                    onChange={(time) => {
-                      setDateTimeData({ ...dateTimeData, startTime: time });
-                      setErrorField({ ...errorField, startTime: null });
-                    }}
-                  />
-                  {errorField.startTime && (
-                    <Typography
-                      sx={{
-                        fontSize: "12px",
-                        fontWeight: "bold",
-                        color: "#FF0000",
-                      }}
-                    >
-                      {errorField.startTime}
-                    </Typography>
-                  )}
-                </LocalizationProvider>
-                {/* duration  */}
-                <LocalizationProvider dateAdapter={AdapterDayjs}>
-                  <TimePicker
-                    label="Duration*"
-                    value={dateTimeData.duration}
-                    views={["hours", "minutes", "seconds"]}
-                    ampm={false}
-                    onChange={(time) => {
-                      setDateTimeData({ ...dateTimeData, duration: time });
-                      setErrorField({ ...errorField, duration: null });
-                    }}
-                  />
-                  {errorField.duration && (
-                    <Typography
-                      sx={{
-                        fontSize: "12px",
-                        fontWeight: "bold",
-                        color: "#FF0000",
-                      }}
-                    >
-                      {errorField.duration}
-                    </Typography>
-                  )}
-                </LocalizationProvider>
-                {/* reccurrence */}
-                <FormControl fullWidth>
-                  <InputLabel id="demo-simple-select-label">
-                    Recurrence Type
-                  </InputLabel>
-                  <Select
-                    labelId="demo-simple-select-label"
-                    id="demo-simple-select"
-                    label="Recurrence Type"
-                    value={props.data.data?.recurrence_type}
-                    disabled
-                  >
-                    <MenuItem value={"not_repeating"}>Non Repeating</MenuItem>
-                    <MenuItem value={"day"}>Day</MenuItem>
-                    <MenuItem value={"week"}>Week</MenuItem>
-                    <MenuItem value={"month"}>Month</MenuItem>
-                  </Select>
-                </FormControl>
+        <DialogContent className="sm:max-w-[425px] sm:max-h-[600px] overflow-y-auto">
+          <DialogHeader>
+            <DialogTitle>Edit Live Class</DialogTitle>
+          </DialogHeader>
+          <form onSubmit={handleSubmitData} className="grid gap-4">
+            {/* Title */}
+            <div className="grid grid-cols-4 items-center gap-4">
+              <Input
+                id="title"
+                placeholder="Title"
+                value={props.data.data?.title}
+                readOnly
+                disabled
+                className="w-full col-span-4"
+              />
+            </div>
 
-                <Button
-                  type="submit"
-                  variant="contained"
-                  sx={{
-                    backgroundColor: "#2059EE",
-                    color: "#fff",
-                    "&:hover": {
-                      backgroundColor: "#2059EE",
-                      color: "#fff",
-                    },
-                  }}
-                >
-                  Edit
-                </Button>
-              </Box>
-            </Box>
-          </Box>
-        </Fade>
-      </Modal>
+            {/* Start Date */}
+            <div className="grid grid-cols-4 items-center gap-2">
+              <Popover>
+                <PopoverTrigger asChild>
+                  <Button
+                    variant={"light"}
+                    className={cn(
+                      "justify-start text-left font-normal w-full col-span-4",
+                      !startDate && "text-muted-foreground"
+                    )}
+                  >
+                    <CalendarIcon className="mr-2 h-4 w-4" />
+                    {startDate ? (
+                      format(startDate, "PPP")
+                    ) : (
+                      <span>Start Date</span>
+                    )}
+                  </Button>
+                </PopoverTrigger>
+                <PopoverContent className="w-auto p-0" align="start">
+                  <Calendar
+                    mode="single"
+                    selected={startDate}
+                    onSelect={setStartDate}
+                    initialFocus
+                  />
+                </PopoverContent>
+              </Popover>
+              {errorField.startDate && (
+                <p className="text-red-500 text-xs col-span-4">
+                  {errorField.startDate}
+                </p>
+              )}
+            </div>
+
+            {/* End Date */}
+            <div className="grid grid-cols-4 items-center gap-2">
+              <Button
+                variant={"light"}
+                className="justify-start text-left font-normal w-full col-span-4 text-muted-foreground"
+                disabled
+              >
+                <CalendarIcon className="mr-2 h-4 w-4" />
+                {props.data.data?.end_date
+                  ? format(new Date(props.data.data.end_date), "PPP")
+                  : "No end date"}
+              </Button>
+            </div>
+
+            {/* Start Time */}
+            <div className="flex flex-col gap-2">
+              <TimePickerAMPM date={startTime} setDate={setStartTime} />
+              {errorField.startTime && (
+                <p className="text-red-500 text-xs col-span-4">
+                  {errorField.startTime}
+                </p>
+              )}
+            </div>
+
+            {/* Duration */}
+            <div className="flex flex-col gap-2">
+              <TimePicker date={duration} setDate={setDuration} />
+              {errorField.duration && (
+                <p className="text-red-500 text-xs col-span-4">
+                  {errorField.duration}
+                </p>
+              )}
+            </div>
+
+            {/* Recurrence */}
+            <div className="grid grid-cols-1 items-center gap-4">
+              <Label className="w-full">Recurrence Type</Label>
+              <Input
+                id="recurrence_type"
+                placeholder="Recurrence Type"
+                value={props.data.data?.recurrence_type}
+                readOnly
+                disabled
+                className="w-full col-span-4"
+              />
+            </div>
+
+            <Button type="submit" variant="primary">
+              Edit
+            </Button>
+          </form>
+        </DialogContent>
+      </Dialog>
 
       <ToastContainer
         position="bottom-right"
@@ -328,7 +236,7 @@ const EditLiveClassModal = (props: EditLiveClassModalProps) => {
         draggable
         theme="dark"
       />
-    </>
+    </Fragment>
   );
 };
 
