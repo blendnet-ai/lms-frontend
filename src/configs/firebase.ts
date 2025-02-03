bb9b517a734eb149ce94cb79fbeb30d85921fd70
Yasir Mansoori <yasir.mansoori000@gmail.com>
2025-02-03 15:37:20 +0530

    Refactor Firebase configuration to use environment variables for improved flexibility and security

--- src/configs/firebase.ts
+++ src/configs/firebase.ts
@@ -5,12 +5,12 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 // Firebase configuration
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: "lms-staging-477ad.firebaseapp.com",
-  projectId: "lms-staging-477ad",
-  storageBucket: "lms-staging-477ad.firebasestorage.app",
-  messagingSenderId: "44068588474",
-  appId: "1:44068588474:web:be4dd88968eb31ef14ab83",
-  measurementId: "G-NH166F2N7Z",
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
